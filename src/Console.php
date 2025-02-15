9e22ef986e16b2e49bd97102615bf121a3daadcb
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2025-02-15 16:49:43 -0500

    Improve handling of Stringable outputs.

--- src/Console.php
+++ src/Console.php
@@ -8,28 +8,28 @@ class Console
 {
     public static function echo(mixed ...$output): void
     {
-        $output = implode(' ', array_map(fn($output) => print_r($output, true), $output));
+        $output = implode(' ', array_map(fn($output) => (is_object($output) && method_exists($output, '__toString')) ? (string) $output : print_r($output, true), $output));
 
         echo $output . PHP_EOL;
     }
 
     public static function log(mixed ...$output): void
     {
-        $output = implode(' ', array_map(fn($output) => print_r($output, true), $output));
+        $output = implode(' ', array_map(fn($output) => (is_object($output) && method_exists($output, '__toString')) ? (string) $output : print_r($output, true), $output));
 
         echo $output . PHP_EOL;
     }
 
     public static function write(mixed ...$output): void
     {
-        $output = implode(' ', array_map(fn($output) => print_r($output, true), $output));
+        $output = implode(' ', array_map(fn($output) => (is_object($output) && method_exists($output, '__toString')) ? (string) $output : print_r($output, true), $output));
 
         echo $output;
     }
 
     public static function writeLine(mixed ...$output): void
     {
-        $output = implode(' ', array_map(fn($output) => print_r($output, true), $output));
+        $output = implode(' ', array_map(fn($output) => (is_object($output) && method_exists($output, '__toString')) ? (string) $output : print_r($output, true), $output));
 
         echo $output . PHP_EOL;
     }
--- src/println.php
+++ src/println.php
@@ -6,7 +6,7 @@ namespace PHPIO;
 
 function println(mixed ...$output): void
 {
-    $output = implode(' ', array_map(fn($output) => print_r($output, true), $output));
+    $output = implode(' ', array_map(fn($output) => (is_object($output) && method_exists($output, '__toString')) ? (string) $output : print_r($output, true), $output));
 
     echo $output . PHP_EOL;
 }
