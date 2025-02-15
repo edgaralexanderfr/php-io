aaa12a95267605f21c41abcbecb985b541087f90
Edgar Alexander Franco <edgaralexanderfr@gmail.com>
2025-02-14 19:39:05 -0500

    Define `Console::write`, `Console::writeLine` and `Console::readLine` methods.

--- src/Console.php
+++ src/Console.php
@@ -12,4 +12,23 @@ class Console
 
         echo $output . PHP_EOL;
     }
+
+    public static function write(mixed ...$output): void
+    {
+        $output = implode(' ', array_map(fn($output) => print_r($output, true), $output));
+
+        echo $output;
+    }
+
+    public static function writeLine(mixed ...$output): void
+    {
+        $output = implode(' ', array_map(fn($output) => print_r($output, true), $output));
+
+        echo $output . PHP_EOL;
+    }
+
+    public static function readLine(?string $prompt = null): string|false
+    {
+        return readline($prompt);
+    }
 }
